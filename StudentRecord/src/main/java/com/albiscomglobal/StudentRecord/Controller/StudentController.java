844f6cea2bbe423d728f4dd162f50111ceaf615a
ahmedwoye@gmail.com Woye1234@@ <ahmedwoye@gmail.com>
2024-03-20 02:25:01 +0000

    controller complete

--- StudentRecord/src/main/java/com/albiscomglobal/StudentRecord/Controller/StudentController.java
+++ StudentRecord/src/main/java/com/albiscomglobal/StudentRecord/Controller/StudentController.java
@@ -5,9 +5,7 @@ import com.albiscomglobal.StudentRecord.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,26 @@ public class StudentController {
     @GetMapping("/student/{id}")
     public ResponseEntity<Student> getSingleStudent(@PathVariable Long id){
         return new ResponseEntity<Student>(studentService.getSingleStudent(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/student")
+    public ResponseEntity<Student> saveSingleStudent(@RequestBody Student student){
+        return new ResponseEntity<Student>(studentService.saveSingleStudent(student), HttpStatus.CREATED);
+    }
+
+
+
+
+    @DeleteMapping("/student/{id}")
+    public void deleteSingleStudent(@RequestParam Long id){
+        studentService.deleteSingleStudent(id);
+    }
+
+
+
+    @PutMapping("/student")
+    public Student updateSingleStudent(@RequestBody Student student , @PathVariable Long id){
+        student.setId(id);
+        return studentService.saveSingleStudent(student);
     }
 }
